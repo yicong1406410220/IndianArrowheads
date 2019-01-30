16a70d593141aeb482ede61e7c6df7821f3443f2
yicong1406410220 <azazaz123456@foxmail.com>
2019-01-30 17:13:46 +0800

    音效处理

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        PlayerPrefs.SetInt("", 1);
+	}
+
+    
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utils {
+
+    public static void SetInt(string key, int value)
+    {
+        PlayerPrefs.SetInt(key, value);
+    }
+
+    public static int GetInt(string key)
+    {
+        return PlayerPrefs.GetInt(key);
+    }
+
+    public static void SetString(string key, string value)
+    {
+        PlayerPrefs.SetString(key, value);
+    }
+
+    public static string GetString(string key)
+    {
+        return PlayerPrefs.GetString(key);
+    }
+
+    public static void SetBool(string key, bool value)
+    {
+        if (value)
+            PlayerPrefs.SetInt(key, 1);
+        else
+            PlayerPrefs.SetInt(key, 0);
+    }
+
+    public static bool GetBool(string key)
+    {
+        int val = PlayerPrefs.GetInt(key);
+        if (val == 1)
+            return true;
+        return false;
+    }
+}
