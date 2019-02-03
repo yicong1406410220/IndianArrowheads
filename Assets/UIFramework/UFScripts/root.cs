a22fac6de1c70e4aece617508362e00556e3bd89
yicong1406410220 <azazaz123456@foxmail.com>
2019-02-03 18:33:07 +0800

    lose界面

--- Assets/UIFramework/UFScripts/root.cs
+++ Assets/UIFramework/UFScripts/root.cs
@@ -8,7 +8,7 @@ public class root : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        PanelMgr.instance.OpenPanel<PlayPanel>("");
+        PanelMgr.instance.OpenPanel<LosePanel>("");
     }
 	
 	// Update is called once per frame
