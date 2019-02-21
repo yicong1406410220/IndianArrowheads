3781d35ec3da2ef56133bc6a0b85838841e47c5f
yicong1406410220 <azazaz123456@foxmail.com>
2019-02-21 16:39:48 +0800

    无用

---
+++ Assets/lookTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lookTest : MonoBehaviour {
+
+    public Transform target;
+
+
+    // Use this for initialization
+    void Start () {
+        // Direction vector towards other object (relative to this transform):
+        var directionTo = (target.transform.position - transform.position).normalized;
+
+        // align forward axis to direction:
+        transform.up = directionTo;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
