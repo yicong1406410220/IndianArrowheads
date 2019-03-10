0cc9ed0abc3cd5295deeb49047ab970c01d37a19
yicong1406410220 <azazaz123456@foxmail.com>
2019-03-10 17:08:39 +0800

    分数

--- Assets/Scripts/UIPanel/WinPanel.cs
+++ Assets/Scripts/UIPanel/WinPanel.cs
@@ -7,16 +7,41 @@ public class WinPanel : PanelBase {
 
     public static WinPanel instance;
 
+    public Text score;
+
+    public GameObject[] Stars;
+
     public override void Init(params object[] args)
     {
         base.Init(args);
         layer = PanelLayer.Panel;
         instance = this;
+        //ÔùËÍ300½ð±Ò
+        PlayerData.AddGold(300);
+        score.text = EntityManager.Instance.GetPlayerMinerEntity().score.ToString();
+        int starCount = EntityManager.Instance.GetPlayerMinerEntity().starCount;
+        SetStarNumber(starCount);
+
         int GateLevel = PlayerPrefs.GetInt("DB_GateLevel");
         if (GateLevel == DataManager.instance.PlayLV)
         {
             GateLevel++;
             PlayerPrefs.SetInt("DB_GateLevel", GateLevel);
+        }
+
+        
+
+    }
+
+    public void SetStarNumber(int value)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Stars[i].SetActive(false);
+        }
+        for (int i = 0; i < value; i++)
+        {
+            Stars[i].SetActive(true);
         }
     }
 
